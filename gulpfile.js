0c46bc0066327ad7e3c171ec62b38ef3e6b27e55
wellfrog <wellfrog16@gmail.com>
2018-03-07 03:12:28 +0800

    更新

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const reload = browserSync.reload;
 const distDev = './dist/dev';
 const distBuild = './dist/build';
 
-const jsFolders = ['utils', 'helper', 'js', 'modules'];
+// const jsFolders = ['utils', 'helper', 'js', 'modules'];
+const jsFolders = { utils : 'utils', helper: 'helper', app: 'app', normal: '@(lib|nls)'};
 
 // 参考vue，es6解析设置
 const presets = [
@@ -46,7 +47,7 @@ gulp.task('watch', () => {
     gulp.watch('./src/style/**/*.less', ['move-css']);
 
     // 监听i18n变化
-    gulp.watch('./src/nls/**/*.js', ['move-i18n']);
+    // gulp.watch('./src/nls/**/*.js', ['move-i18n']);
 
     // 监听image变化
     gulp.watch('./src/assets/img/**/*.*', ['move-image']);
@@ -58,14 +59,14 @@ gulp.task('watch', () => {
     gulp.watch('./src/assets/video/**/*.*', ['move-video']);
 
     // 监听js模块变化
-    for (const key of jsFolders) {
-        gulp.watch(`./src/${key}/**/*.*`, [`move-${key}`]);
+    for (const key in jsFolders) {
+        gulp.watch(`./src/js/${jsFolders[key]}/**/*.*`, [`move-${key}`]);
     }
 });
 
 gulp.task('dev', (cb) => {
-    // $.sequence('clean-dev', ['move-html', 'move-modules', 'move-utils', 'move-helper', 'move-js'], 'server')(cb);
-    $.sequence('clean-dev', ['move-html', 'move-modules', 'move-utils', 'move-helper', 'move-js', 'move-i18n', 'move-css', 'move-image', 'move-audio', 'move-video'], 'server')(cb);
+    // $.sequence('clean-dev', ['move-html', 'move-modules', 'move-utils', 'move-helper', 'move-js', 'move-i18n', 'move-css', 'move-image', 'move-audio', 'move-video'], 'server')(cb);
+    $.sequence('clean-dev', ['move-html', 'move-normal', 'move-utils', 'move-helper', 'move-app', 'move-css', 'move-image', 'move-audio', 'move-video'], 'server')(cb);
 });
 
 // 移动html
@@ -122,19 +123,38 @@ gulp.task('move-modules', () =>
 );
 
 // 移动和eslint：utils, helper, js
-for (const key of jsFolders.slice(0, 3)) {
+// for (const key of jsFolders.slice(0, 3)) {
+//     // 移动
+//     gulp.task(`move-${key}`, [`eslint-${key}`], () => 
+//         gulp.src(`./src/${key}/**/*.*`)
+//             .pipe($.cache($.babel({ presets })))
+//             .pipe($.changed(`${distDev}/${key}`))
+//             .pipe(gulp.dest(`${distDev}/${key}`))
+//             .pipe(reload({stream: true}))
+//     );
+
+//     // eslint检查
+//     gulp.task(`eslint-${key}`, () => 
+//         gulp.src(`./src/${key}/**/*.*`)
+//             .pipe($.eslint())
+//             .pipe($.eslint.formatEach())
+//             .pipe($.eslint.failAfterError())
+//     );
+// }
+for (const key in jsFolders) {
+    if (key === 'normal') { continue; }
     // 移动
     gulp.task(`move-${key}`, [`eslint-${key}`], () => 
-        gulp.src(`./src/${key}/**/*.*`)
+        gulp.src(`./src/js/${jsFolders[key]}/**/*.js`)
             .pipe($.cache($.babel({ presets })))
-            .pipe($.changed(`${distDev}/${key}`))
-            .pipe(gulp.dest(`${distDev}/${key}`))
+            .pipe($.changed(`${distDev}/js/${key}`))
+            .pipe(gulp.dest(`${distDev}/js/${key}`))
             .pipe(reload({stream: true}))
     );
 
     // eslint检查
     gulp.task(`eslint-${key}`, () => 
-        gulp.src(`./src/${key}/**/*.*`)
+        gulp.src(`./src/${jsFolders[key]}/**/*.js`)
             .pipe($.eslint())
             .pipe($.eslint.formatEach())
             .pipe($.eslint.failAfterError())
