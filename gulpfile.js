658e48c5626bbee77f1a190af77eb9fa6f3f94f1
wellfrog <wellfrog16@gmail.com>
2018-04-11 03:11:33 +0800

    调整gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -258,7 +258,7 @@ gulp.task('htmlreplace', () =>
         .pipe($.revHash({assetsDir: distBuild}))
         .pipe($.htmlmin({
             removeComments: true,
-            collapseWhitespace: false
+            collapseWhitespace: true
         }))
         .pipe(gulp.dest(`${distBuild}`))
 );
